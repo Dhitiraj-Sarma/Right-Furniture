a4a2903dd1432f25a4abbb93fcb515b4d38eb16b
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2024-10-11 17:58:53 +0530

    right furniture

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
 
-import wishlistReducer from "./slices/wishlistSlice";
 import wishlistSlice from "./slices/wishlistSlice";
 
 const store = configureStore({
