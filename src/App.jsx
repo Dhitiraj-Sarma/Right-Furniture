e0a467b071a8c25609b98e7fd3394fbb2847a599
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2024-10-14 13:07:42 +0530

    right furniture

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
+
 import Navbar from "./Components/Navbar";
-import Shop from "./Pages/Shop";
-import Cart from "./Pages/Cart";
-import Wishlist from "./Pages/Wishlist";
+// import Shop from "./Pages/Shop";
+// import Cart from "./Pages/Cart";
+// import Wishlist from "./Pages/Wishlist";
 import Footer from "./Components/Footer";
+import React, { Suspense } from "react";
 
 function App() {
+  const Home = React.lazy(() => import("./Pages/Home"));
+  const Shop = React.lazy(() => import("./Pages/Shop"));
+  const Cart = React.lazy(() => import("./Pages/Cart"));
+  const Wishlist = React.lazy(() => import("./Pages/Wishlist"));
+
   return (
     <Router>
       <div>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
